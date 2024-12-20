bf3bdccff39104bb6de0482a7af2d40005ffd9f3
Hafiz <hasnathafiz229@gmail.com>
2024-12-20 11:27:24 -0600

    Time: 7 ms (69.72%) | Memory: 50.6 MB (70.47%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -10,18 +10,35 @@ var isIsomorphic = function(s, t) {
     const tMap = new Map();
 
     for(let i=0; i<s.length; i++) {
-        if(sMap.has(s[i])) {
-            if(sMap.get(s[i]) !== t[i]) return false;
-        } else {
-            sMap.set(s[i], t[i]);
-        }
+        const sChar = s[i];
+        const tChar = t[i];
 
-        if(tMap.has(t[i])) {
-            if(tMap.get(t[i]) !== s[i]) return false;
-        } else {
-            tMap.set(t[i], s[i]);
-        }
+        if(sMap.get(sChar) !== tMap.get(tChar)) return false;
+
+        sMap.set(sChar, i);
+        tMap.set(tChar, i);
     }
 
     return true;
+
+    // if(s.length !== t.length) return false;
+
+    // const sMap = new Map();
+    // const tMap = new Map();
+
+    // for(let i=0; i<s.length; i++) {
+    //     if(sMap.has(s[i])) {
+    //         if(sMap.get(s[i]) !== t[i]) return false;
+    //     } else {
+    //         sMap.set(s[i], t[i]);
+    //     }
+
+    //     if(tMap.has(t[i])) {
+    //         if(tMap.get(t[i]) !== s[i]) return false;
+    //     } else {
+    //         tMap.set(t[i], s[i]);
+    //     }
+    // }
+
+    // return true;
 };
