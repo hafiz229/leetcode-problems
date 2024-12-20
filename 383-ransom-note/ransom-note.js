e72cddf28fd8a71ada462882d4efba0277afc687
Hafiz <hasnathafiz229@gmail.com>
2024-12-20 10:37:15 -0600

    Time: 0 ms (100.00%) | Memory: 53 MB (41.90%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -4,11 +4,22 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    for(let i=0; i<ransomNote.length; i++) {
-        const currentIndex = magazine.indexOf(ransomNote[i]);
-        if(currentIndex === -1) return false;
-        magazine = magazine.substring(0, currentIndex) + magazine.substring(currentIndex + 1);
-    }
+    const ransomNoteArr = ransomNote.split("");
+    const magazineArr = magazine.split("");
 
-    return true;
+    return ransomNoteArr.every((element) => {
+        const index = magazineArr.indexOf(element);
+        if(index === -1) return false;
+
+        delete magazineArr[index];
+        return true;
+    })
+
+    // for(let i=0; i<ransomNote.length; i++) {
+    //     const currentIndex = magazine.indexOf(ransomNote[i]);
+    //     if(currentIndex === -1) return false;
+    //     magazine = magazine.substring(0, currentIndex) + magazine.substring(currentIndex + 1);
+    // }
+
+    // return true;
 };
