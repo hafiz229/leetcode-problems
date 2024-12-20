93d575aaa14704494ecbc45a33e73f344a2f581f
Hafiz <hasnathafiz229@gmail.com>
2024-12-20 11:32:25 -0600

    Time: 9 ms (44.24%) | Memory: 50.4 MB (82.45%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -6,21 +6,38 @@
 var isIsomorphic = function(s, t) {
     if(s.length !== t.length) return false;
 
-    const sMap = new Map();
-    const tMap = new Map();
+    let sObj = {};
+    let tObj = {};
 
     for(let i=0; i<s.length; i++) {
-        const sChar = s[i];
-        const tChar = t[i];
+        if(!(s[i] in sObj)) sObj[s[i]] = i;
 
-        if(sMap.get(sChar) !== tMap.get(tChar)) return false;
+        if(!(t[i] in tObj)) tObj[t[i]] = i;
 
-        sMap.set(sChar, i);
-        tMap.set(tChar, i);
+        if(sObj[s[i]] !== tObj[t[i]]) return false;
     }
 
     return true;
 
+    // 2nd Approach
+    // if(s.length !== t.length) return false;
+
+    // const sMap = new Map();
+    // const tMap = new Map();
+
+    // for(let i=0; i<s.length; i++) {
+    //     const sChar = s[i];
+    //     const tChar = t[i];
+
+    //     if(sMap.get(sChar) !== tMap.get(tChar)) return false;
+
+    //     sMap.set(sChar, i);
+    //     tMap.set(tChar, i);
+    // }
+
+    // return true;
+
+    // 1st Approach
     // if(s.length !== t.length) return false;
 
     // const sMap = new Map();
