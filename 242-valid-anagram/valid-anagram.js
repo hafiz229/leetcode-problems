bd37c8d85f44f9b1142ccde05668c8709c7c38c9
Hafiz <hasnathafiz229@gmail.com>
2024-12-23 13:53:32 -0600

    Time: 13 ms (76.81%) | Memory: 50.9 MB (87.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,5 +4,35 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split("").sort().join() === t.split("").sort().join();
+    if(s.length !== t.length) return false;
+
+    const sMap = new Map();
+
+    for(let i=0; i<s.length; i++) {
+        if(sMap.has(s[i])) {
+            sMap.set(s[i], sMap.get(s[i]) + 1);
+        } else {
+            sMap.set(s[i], 1)
+        }
+    }
+
+    for(let i=0; i<t.length; i++) {
+        if(!sMap.has(t[i]) || (sMap.has(t[i]) && sMap.get(t[i]) === 0)) return false;
+        sMap.set(t[i], sMap.get(t[i]) - 1);
+    }
+
+    return true;
+
+    // return s.split("").sort().join() === t.split("").sort().join();
+    // const arr = new Array(26);
+    // for(let i=0; i<s.length; i++) {
+    //     arr[s.charCodeAt(i) - 97]++;
+    // }
+
+    // for(let i=0; i<t.length; i++) {
+    //     if(arr[t.charCodeAt(i) - 97]-- === 0) return false;
+    // }
+
+    // return arr.every(element => element === 0);
+
 };
