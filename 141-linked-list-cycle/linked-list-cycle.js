f01528bad2e1a2bfeba3080f6e914c21fe98e1c4
Hafiz <hasnathafiz229@gmail.com>
2025-01-02 16:18:19 -0600

    Time: 59 ms (82.91%) | Memory: 52.6 MB (88.28%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -14,18 +14,17 @@ var hasCycle = function(head) {
     if(head === null) return false;
 
     let slow = head;
-    let fast = head.next;
+    let fast = head;
 
-    while(slow != fast) {
-        if(fast === null || fast.next == null) return false;
-
+    while(fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
+
+        if(slow === fast) return true;
     }
 
-    return true;
+    return false;
 
-    return true;
     // Approach 01
     // const set = new Set();
     // let current = head;
